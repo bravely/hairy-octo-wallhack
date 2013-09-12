cc66a7566ced2e49fbed7244d2d4f6232db4169b
Jake Mays <quaunaut@gmail.com>
2013-09-12 14:45:59 -0500

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,35 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'anjlab-bootstrap-rails'
+gem 'pacecar'
+gem 'rails-console-tweaks'
+gem 'pg'
+gem 'pry-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'rubocop'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+  gem 'simplecov', require: false
+  gem 'webrat'
+  gem 'should_not'
+  gem 'factory_girl_rspec'
+end
+
+gem :ct do
+  gem 'guard-rspec'
+  gem 'guard-rubocop'
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HairyOctoWallhack::Application.config.secret_key_base = '580efe0160382e731565ba283d1d18550c8d87b4aa7dd2579c3f30aa570e19d7cfbe7e65067a503f66a60d37b1f5814c92276319af39053e71dd4f2c1acdb88b'
