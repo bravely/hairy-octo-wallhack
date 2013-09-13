1d9a91efcf6bdb1eaf2716053bc05db369ddc651
Jake Mays <quaunaut@gmail.com>
2013-09-13 14:27:25 -0500

    We're ember.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 HairyOctoWallhack::Application.routes.draw do
+  get "static/index"
+
+  root to: 'static#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
